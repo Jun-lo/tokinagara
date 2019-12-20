dd42f4c4a0767fd725c078a31fa53e60b4b97ac8
jun-lo <fe.lolo.j@ezweb.ne.jp>
2019-12-20 23:13:12 +0900

    first practice

--- ex.0206.rb
+++ ex.0206.rb
@@ -2,7 +2,7 @@
 
 # キーボードから読み込んだ整数値に10を加えた値と10を減じた値を出力するプログラム　
 
-numbers = 7
+numbers = gets.to_i
 
 puts "10を加えた値は#{numbers+10}です。"
 puts "10を減じた値は#{numbers-10}です。"
--- ex.0209.rb
+++ ex.0209.rb
@@ -1,21 +1,17 @@
 # -*- coding: utf-8 -*-
 
 # 三角形の底辺と高さを読み込んで、その面積を表示するプログラム
-input = gets
-n = input.to_f
 
-p "底辺#{}"
+n = gets.to_f
+y = gets.to_f
 
+print  "底辺：", n
+print "高さ：", y
 
-print "三角形の面積を求めます"
-
-
-
-
-width = 6.5
-height = 3.5
 
 puts "三角形の面積を求めます。"
-puts "底辺#{width}"
-puts "高さ#{height}"
-puts "面積は#{width*height/2}です。"
+
+print "面積は#{n*y/2}です"
+
+
+
