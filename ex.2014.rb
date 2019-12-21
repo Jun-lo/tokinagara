6a3462c9d9a6469950bd27f64e5eef669c916707
jun-lo <fe.lolo.j@ezweb.ne.jp>
2019-12-21 11:41:49 +0900

    first practice

---
+++ ex.2014.rb
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# 名前の姓と名とを個別にキーボードから読み込んで、挨拶を行うプログラム
+
+name1 = gets
+name2 = gets
+
+print "姓：",name1
+
+print "名：",name2
+
+
+print "こんにちは#{name1+name2}です。"
+
+
+
---
+++ ex.2015.rb
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# 住所を読み込んで表示を行うプログラム
+
+address = gets
+
+puts "住所：",address
+
+print "お住いは#{address}ですね。"
+
