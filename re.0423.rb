dd8c0988cfbae4fdc8fe572e0a015a35dbaa02aa
jun-lo <fe.lolo.j@ezweb.ne.jp>
2020-01-30 21:56:25 +0900

    second practice

---
+++ re.0423.rb
@@ -0,0 +1,22 @@
+#-*- coding: utf-8 -*-
+
+#n段のピラミッドを表示するプログラム
+# 第i行目には(i - 1) * 2 + 1個の'*'記号を表示して、最終行である第n行目には（n - 1) * 2 + 1個の'*'を表示する。
+
+puts "ピラミッドを表示します。"
+print "段数は："
+
+input = gets
+n = input.to_i
+
+for i in 1..n
+  for k in Array.new(n - i)
+    print " "
+    end
+    for k in 0..(2*(i - 1))
+      print "*"
+    end
+
+
+puts
+end
---
+++ re.0424.rb
@@ -0,0 +1,20 @@
+#-*- coding: utf-8 -*-
+
+#n段のピラミッドを表示するプログラム
+# 第i行目にはi % 10を表示する
+
+puts "数字ピラミッドを表示します。"
+print "段数は："
+
+input = gets
+n = input.to_i
+
+for i in 1..n
+  for k in Array.new(n - i)
+    print " "
+  end
+  for k in 0..(2*(i - 1))
+  print i % 10
+  end
+  puts
+end
